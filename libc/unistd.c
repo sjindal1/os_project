9d0d479191371b8d42c01467eb89cdf9cb3fdd7a
Sahil <sahil.jindal@stonybrook.edu>
2017-12-06 01:17:06 -0500

    sleep unistd implementation

---
+++ libc/unistd.c
@@ -0,0 +1,14 @@
+#include <sys/stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
+unsigned int sys_sleep();
+
+unsigned int sleep(unsigned int seconds){
+	unsigned int cur_time = sys_sleep();
+	unsigned int time = cur_time;
+  while(time <= cur_time + seconds){
+  	time = sys_sleep();
+  }
+  return time;
+}
